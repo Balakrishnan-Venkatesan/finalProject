cc0e9a330305600a1c88e7b0f6761806b42af624
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-15 19:14:01 -0500

    modify

--- mvc/pages/homepage.php
+++ mvc/pages/homepage.php
@@ -17,8 +17,8 @@
 
 <body>
 
+<h1><div style="text-align:center">
 
-<h1>
     <?php
 
     //this how to print some data;
@@ -26,12 +26,12 @@
 
     ?> </h1>
 
-<h1><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
-<h1><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
+<h1><div style="text-align:center"><a href="index.php?page=accounts&action=all">Show All Accounts</a></h1>
+<h1><div style="text-align:center"><a href="index.php?page=tasks&action=all">Show All Tasks</a></h1>
 
 <form action="index.php?page=accounts&action=login" method="POST">
 
-    <div class="container">
+    <div class="container"><div style="text-align:center">
         <label><b>Username</b></label>
         <input type="text" placeholder="Enter Username" name="email" required>
 
@@ -43,7 +43,7 @@
 
 
 </form>
-<h1><a href="index.php?page=accounts&action=register">Register</a></h1>
+<h1><div style="text-align:center"><a href="index.php?page=accounts&action=register">Register</a></h1>
 
 
 <script src="js/scripts.js"></script>
