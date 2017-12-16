f10875397dc7a3ce85bafdfe5e4bce5e797f1773
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-16 03:54:15 -0500

    update accounts view

--- mvc/pages/all_accounts.php
+++ mvc/pages/all_accounts.php
@@ -18,16 +18,12 @@
 <body>
 <h1>All Accounts</h1>
 
-<h1> HI</h1>
-
 <?php
 //this is how you print something
 
 print utility\htmlTable::genarateTableFromMultiArray($data);
 
-
 ?>
-
 
 <script src="js/scripts.js"></script>
 </body>
