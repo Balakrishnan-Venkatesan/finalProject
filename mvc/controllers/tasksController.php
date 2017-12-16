01e916033400dd2d0cb4350437b6948496906387
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-16 18:00:03 -0500

    added new pg

--- mvc/controllers/tasksController.php
+++ mvc/controllers/tasksController.php
@@ -14,8 +14,8 @@ class tasksController extends http\controller
     //to call the show function the url is index.php?page=task&action=show
     public static function show()
     {
-        $record = todos::findOne($_REQUEST['id']);
-        self::getTemplate('show_task', $record);
+        $todo = todos::findOne($_REQUEST['id']);
+        self::getTemplate('createtodo_edit', $todo);
     }
     
     public static function newForm()
