e310d9a9842039232c259396414937173c776dde
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-17 13:36:24 -0500

    edits in pg linking

--- mvc/controllers/accountsController.php
+++ mvc/controllers/accountsController.php
@@ -56,7 +56,7 @@ class accountsController extends http\controller
             //you may want to send the person to a
             // login page or create a session and log them in
             // and then send them to the task list page and a link to create tasks
-            header("Location: index.php?page=accounts&action=all");
+            header("Location: index.php");
 
         } else {
             //You can make a template for errors called error.php
@@ -94,7 +94,7 @@ class accountsController extends http\controller
     {
         $record = accounts::findOne($_REQUEST['id']);
         $record->delete();
-        header("Location: index.php?page=accounts&action=all");
+        header("Location: index.php");
     }
     
     public static function logout()
