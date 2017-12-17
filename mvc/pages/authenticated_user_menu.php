cccd6417993add5e9a45460cc82727ec88d285af
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-17 17:49:42 -0500

    bootstrap navbar

--- mvc/pages/authenticated_user_menu.php
+++ mvc/pages/authenticated_user_menu.php
@@ -1,10 +1,29 @@
-<a href="index.php?page=tasks&action=create">Create task<br><br></a>
-<a href="index.php?page=accounts&action=all">All Accounts<br><br></a>
-<a href="index.php?page=accounts&action=show">Your Account<br><br></a>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <h4><a class="brand" href="#"><font color="black">TODOS</font></a></h4>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php?page=accounts&action=all"><font color="black">All Accounts</font><span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?page=accounts&action=show"><font color="black">Your Account</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="index.php?page=tasks&action=create"><font color="black">Create Task</font></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="index.php?page=accounts&action=logout"><font color="black">Logout</font></a>
+      </li>
+    </ul>
+  </div>
+</nav>
 
 <?php
 //this is how you print something
-  echo 'List of tasks created';
+  echo '<br>List of tasks created';
 
   if ($data == false) {
     echo '<p> no tasks found </p>';
@@ -13,5 +32,3 @@
   }   
 
 ?>
-
-<a href="index.php?page=accounts&action=logout"><br>Logout<br><br></a>
