546a2d4dd93e1d0143ccc595a5318cf7418aa5ff
Balakrishnan Venkatesan <bv98@afsconnect1.njit.edu>
2017-12-17 23:47:58 -0500

    modify

--- mvc/pages/all_tasks.php
+++ mvc/pages/all_tasks.php
@@ -1,28 +1,5 @@
-<!-- <!doctype html>
-
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-
-    <title>Welcome to TODOS creator</title>
-    <meta name="description" content="Welcome to TODOS creator">
-    <meta name="author" content="SitePoint">
-
-    <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-    <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-    <![endif]
-</head>
-
-<body> -->
-
 <?php include 'header.php'; ?>
 
 <?php include 'authenticated_user_menu.php'; ?>
 
 <?php include 'footer.php'; ?>
-
-<!-- <script src="js/scripts.js"></script>
-</body>
-</html> -->
